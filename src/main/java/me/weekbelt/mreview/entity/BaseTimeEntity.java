7b645c536d333024b6d57ff614d6a5ac728ddb3e
weekbelt <vfrvfr4207@gmail.com>
2021-01-24 00:26:58 +0900

    feat: Create Movie, MovieImage entity

---
+++ src/main/java/me/weekbelt/mreview/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package me.weekbelt.mreview.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime modDate;
+
+}
