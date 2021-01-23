e4322f8441463bfc231e45a78d46df05925ccc17
weekbelt <vfrvfr4207@gmail.com>
2021-01-24 00:18:07 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/mreview/MreviewApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.mreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MreviewApplication.class, args);
+    }
+
+}
