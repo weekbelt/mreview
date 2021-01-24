a5a2c67fe2abb976b5eef0be2e5ba66f6aa242e2
weekbelt <vfrvfr4207@gmail.com>
2021-01-24 16:58:28 +0900

    feat: Create getListPage, getMovieWithAll in Movie

--- src/main/java/me/weekbelt/mreview/repository/MovieRepository.java
+++ src/main/java/me/weekbelt/mreview/repository/MovieRepository.java
@@ -1,8 +1,22 @@
 package me.weekbelt.mreview.repository;
 
+import java.util.List;
 import me.weekbelt.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @Query("select m, max(mi), avg(coalesce(r.grade,0)), count(distinct r) from Movie m " +
+        "left outer join MovieImage mi on mi.movie = m " +
+        "left outer join Review r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable);
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(r) "
+        + "from Movie m left outer join MovieImage mi on mi.movie = m "
+        + "left outer join Review r on r.movie = m "
+        + "where m.mno = :mno group by mi")
+    List<Object[]> getMovieWithAll(Long mno);
 }
--- src/test/java/me/weekbelt/mreview/repository/MovieRepositoryTest.java
+++ src/test/java/me/weekbelt/mreview/repository/MovieRepositoryTest.java
@@ -1,5 +1,7 @@
 package me.weekbelt.mreview.repository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 import me.weekbelt.mreview.entity.Movie;
@@ -7,6 +9,10 @@ import me.weekbelt.mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,31 +25,56 @@ class MovieRepositoryTest {
     @Autowired
     private MovieImageRepository movieImageRepository;
 
-    @Commit
-    @Transactional
+//    @Commit
+//    @Transactional
+//    @Test
+//    public void insertMovies() {
+//        IntStream.rangeClosed(1, 100).forEach(i -> {
+//            Movie movie = Movie.builder()
+//                .title("Movie...." + i)
+//                .build();
+//
+//            System.out.println("------------------------");
+//
+//            movieRepository.save(movie);
+//
+//            int count = (int) (Math.random() * 5) + 1;   // 1, 2, 3, 4
+//
+//            for (int j = 0; j < count; j++) {
+//                MovieImage movieImage = MovieImage.builder()
+//                    .uuid(UUID.randomUUID().toString())
+//                    .movie(movie)
+//                    .imgName("test" + j + ".jpg")
+//                    .build();
+//                movieImageRepository.save(movieImage);
+//            }
+//
+//            System.out.println("-----------------------");
+//        });
+//    }
+
     @Test
-    public void insertMovies() {
-        IntStream.rangeClosed(1, 100).forEach(i -> {
-            Movie movie = Movie.builder()
-                .title("Movie...." + i)
-                .build();
+    public void testListPage() {
+        // given
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Direction.DESC, "mno"));
 
-            System.out.println("------------------------");
+        // when
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
 
-            movieRepository.save(movie);
+        // then
+        List<Object[]> content = result.getContent();
+        content.forEach(objects -> System.out.println(Arrays.toString(objects)));
+    }
 
-            int count = (int) (Math.random() * 5) + 1;   // 1, 2, 3, 4
+    @Test
+    public void testGetMovieWithAll() {
+        // given
+        List<Object[]> result = movieRepository.getMovieWithAll(94L);
 
-            for (int j = 0; j < count; j++) {
-                MovieImage movieImage = MovieImage.builder()
-                    .uuid(UUID.randomUUID().toString())
-                    .movie(movie)
-                    .imgName("test" + j + ".jpg")
-                    .build();
-                movieImageRepository.save(movieImage);
-            }
+        // when
+        System.out.println(result);
 
-            System.out.println("-----------------------");
-        });
+        // then
+        result.forEach(objects -> System.out.println(Arrays.toString(objects)));
     }
 }
