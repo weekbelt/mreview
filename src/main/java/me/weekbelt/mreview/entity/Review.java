d4a3e75192ca1d5bc96d14aa85a8563e2d082df4
weekbelt <vfrvfr4207@gmail.com>
2021-01-24 15:36:21 +0900

    feat: Create Review, Member Entity

---
+++ src/main/java/me/weekbelt/mreview/entity/Review.java
@@ -0,0 +1,35 @@
+package me.weekbelt.mreview.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"movie", "member"})
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewnum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private int grade;
+
+    private String text;
+}
